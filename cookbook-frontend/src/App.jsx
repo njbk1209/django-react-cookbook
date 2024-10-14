cb212c60c2117bd1ab160e8e12e0cb9d9c23edfc
Nelson Bolaño <njbk1996@gmail.com>
2024-10-14 11:50:59 -0400

    Redux Configuration
    Initial redux configuration has been made to the project

--- cookbook-frontend/src/App.jsx
+++ cookbook-frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { Provider } from 'react-redux'
+import store from './store'
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <h1>
         CookBook app
       </h1>
-    </>
+    </Provider>
   )
 }
 
